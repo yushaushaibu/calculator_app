11e4da62c58b36f994a655bbf033aadf298c71d1
unknown <yushaushaibu1@gmail.com>
2023-06-10 11:51:32 +0000

    loop over operands and assign events to each click

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const form = document.getElementById("calc_form");
 const output = document.getElementById("output");
-const operand_btn = document.querySelectorAll("button[data-type=operand]");
+const operand_btns = document.querySelectorAll("button[data-type=operand]");
 
 let is_operator = false;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-});
+})
 
-// loop over operand buttons with click event that displays button number
-operand_btn.forEach((btn) => {
+// Loop over operand buttons with click event that displays button number
+operand_btns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         if (output.value === "0") {
             output.value = e.target.value;
@@ -17,9 +17,9 @@ operand_btn.forEach((btn) => {
             is_operator = false;
             output.value = e.target.value;
         } else if (output.value.includes(".")) {
-            output.value = output.value + "" + e.target.value.replace(".", "")
+            output.value = output.value + "" + e.target.value.replace(".", "");
         } else {
             output.value = output.value + "" + e.target.value;
         }
-    })
+    });
 });
